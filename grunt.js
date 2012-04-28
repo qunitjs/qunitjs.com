d6295c91446437c024c6b213bd42dbac1516d08f
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-04-28 02:13:58 +0200

    Convert export.coffee to grunt task

--- grunt.js
+++ grunt.js
@@ -1,4 +1,3 @@
-// TODO add html validation?
 module.exports = function( grunt ) {
 
 grunt.loadNpmTasks( "grunt-wordpress" );
@@ -8,21 +7,69 @@ grunt.initConfig({
 	wordpress: grunt.file.readJSON( "config.json" ),
 	htmllint: {
 		out: "out/*.html"
+	},
+	lint: {
+		grunt: "grunt.js"
+	},
+	jshint: {
+		options: {
+			undef: true,
+			node: true
+		}
 	}
 });
 
+function scriptHeader( document ) {
+	return "<script>{\n" +
+		"\t\"title\": \"" + document.attributes.title + "\"\n" +
+		"}</script>\n\n";
+}
+
 grunt.registerTask( "build", "Render documents without layout using docpad-render", function() {
-	var done = this.async();
-	grunt.utils.spawn({
-		cmd: "coffee",
-		args: [ "export.coffee" ]
-	}, function(error, result) {
-		if (error) {
-			grunt.log.error( error.stdout );
+	var docpadInstance,
+		done = this.async(),
+		docpad = require( 'docpad' ),
+		fs = require( 'fs' ),
+		path = require( 'path' ),
+		mkdirp = require('mkdirp').sync,
+		config = {
+			outNoLayoutsPath: path.join( __dirname, 'dist/page' )
+		};
+
+	// Create required directories
+	mkdirp(config.outNoLayoutsPath + '/static');
+
+	// Create DocPad, and wait for it to load
+	docpadInstance = docpad.createInstance(config, function( err ) {
+		if ( err ) {
+			throw err;
 		}
-		grunt.log.writeln( "Done: " + result );
-		done();
+
+		// Generate the website
+		docpadInstance.action( 'generate', function( err ) {
+			if ( err ) {
+				throw err;
+			}
+
+			// Save all the documents somewhere without their layouts
+			docpadInstance.documents.forEach( function( document ) {
+				// Prepare
+				var contentRenderedWithoutLayouts = document.get('contentRenderedWithoutLayouts'),
+					outNoLayoutsPath = path.join(config.outNoLayoutsPath, document.get('relativePath')).replace(/\.eco$/, ''),
+					// Save the file
+					result = fs.writeFileSync(outNoLayoutsPath, scriptHeader( document ) + contentRenderedWithoutLayouts);
+
+				if ( result instanceof Error ) {
+					throw result;
+				}
+			});
+
+			grunt.log.writeln( 'Generated' );
+			done();
+		});
 	});
 });
+
+grunt.registerTask( "default", "lint" );
 
 };
