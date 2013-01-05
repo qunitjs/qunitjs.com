48c592aa351ab41c007a103bccb33f8d1fa0e2bd
Richard D. Worth <rdworth@gmail.com>
2013-01-05 06:48:20 -0600

    Intro code samples: tweaked line widths a bit more in a few files. #29

--- resources/intro/prettydate2.js
+++ resources/intro/prettydate2.js
@@ -10,12 +10,15 @@ var prettyDate = {
 		return day_diff === 0 && (
 				diff < 60 && "just now" ||
 				diff < 120 && "1 minute ago" ||
-				diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+				diff < 3600 && Math.floor( diff / 60 ) +
+					" minutes ago" ||
 				diff < 7200 && "1 hour ago" ||
-				diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+				diff < 86400 && Math.floor( diff / 3600 ) +
+					" hours ago") ||
 			day_diff === 1 && "Yesterday" ||
 			day_diff < 7 && day_diff + " days ago" ||
-			day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+			day_diff < 31 && Math.ceil( day_diff / 7 ) +
+				" weeks ago";
 	},
 
 	update: function(now) {
