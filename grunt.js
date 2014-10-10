76ae93e660cf934c2f15f160448ebce8e95f9095
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-10-10 16:51:21 +0200

    Pages: Upgrade guide to QUnit 2.x
    Adds a separate task to post-process the markdown page, putting the generated
    table of contents (ToC) into a sidebar.
    
    Fixes #86
    Closes #88

--- grunt.js
+++ grunt.js
@@ -22,7 +22,16 @@ grunt.registerTask( "clean", function() {
 	rimraf.sync( "dist" );
 });
 
-grunt.registerTask( "build", "build-pages build-resources" );
-grunt.registerTask( "build-wordpress", "check-modules clean build" );
+// Inserts markup to put ToC in sidebar
+grunt.registerTask( "generate-columns", function() {
+	var upgradeGuide = "dist/wordpress/posts/page/upgrade-guide-2.x.html",
+		content = grunt.file.read( upgradeGuide )
+			.replace( /(<\/script>)/, "$1\n<div class=\"four columns\">" )
+			.replace( /(<h2)/, "</div>\n<div class=\"eight columns\">\n$1" );
+	grunt.file.write( upgradeGuide, content );
+});
+
+grunt.registerTask( "build", "build-pages generate-columns build-resources" );
+grunt.registerTask( "build-wordpress", "check-modules clean build");
 
 };
