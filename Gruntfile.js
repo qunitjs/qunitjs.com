3ef62449c6ffe96b464633ce3984dae4980a9c0a
Scott González <scott.gonzalez@gmail.com>
2014-12-08 13:37:23 -0500

    Build: Upgrade to grunt-jquery-content 2.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,10 @@
-var rimraf = require( "rimraf" );
-
 module.exports = function( grunt ) {
 
-grunt.loadNpmTasks( "grunt-check-modules" );
 grunt.loadNpmTasks( "grunt-jquery-content" );
 
 grunt.initConfig({
-	"build-pages": {
-		all: "pages/**"
+	"build-posts": {
+		page: "pages/**"
 	},
 	"build-resources": {
 		all: "resources/**"
@@ -19,10 +16,6 @@ grunt.initConfig({
 	})()
 });
 
-grunt.registerTask( "clean", function() {
-	rimraf.sync( "dist" );
-});
-
 // Inserts markup to put ToC in sidebar
 grunt.registerTask( "generate-columns", function() {
 	var upgradeGuide = "dist/wordpress/posts/page/upgrade-guide-2.x.html",
@@ -32,7 +25,6 @@ grunt.registerTask( "generate-columns", function() {
 	grunt.file.write( upgradeGuide, content );
 });
 
-grunt.registerTask( "build", [ "build-pages", "generate-columns", "build-resources" ] );
-grunt.registerTask( "build-wordpress", [ "check-modules", "clean", "build" ] );
+grunt.registerTask( "build", [ "build-posts", "generate-columns", "build-resources" ] );
 
 };
