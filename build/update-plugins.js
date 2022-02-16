d6a5e4d602ccf2bdfa73fe1cca66d57794f23f89
Timo Tijhof <krinkle@fastmail.com>
2022-02-16 13:18:02 +0000

    Plugins: Refresh npm data

--- build/update-plugins.js
+++ build/update-plugins.js
@@ -7,7 +7,7 @@ const { keyword } = require('./npm-search.js');
 		name: plugin.name,
 		description: plugin.description,
 		date: plugin.date
-	}));
+	})).sort((a, b) => a.name < b.name ? -1 : 1);
 	fs.writeFileSync('./_data/plugins.json', JSON.stringify(plugins, null, 2) + '\n');
 	console.log('Done!');
 }());
